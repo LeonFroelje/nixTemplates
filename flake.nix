48ef391bcbcdc445e80a51eea5b6409139f8e44a
leon froelje <Leon.froelje@uni-bremen.de>
2026-01-20 08:20:05 +0100

    initial commit & typst dev shell

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    templates = {
+      typst = {
+        path = ./typst;
+        description = "Typst devShells in for different languages";
+        welcomeText = "";
+      };
+    };
+
+
+  };
+}
---
+++ typst/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Typst devShells for specific languages";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixvim = {
+          url = "github:nix-community/nixvim";
+          # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
+          inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, nixvim }:
+  let
+    languages = [ "de-DE" "en-US" "en-GB" ];
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in 
+  {
+    nixosModules = builtins.listToAttrs (pkgs.lib.lists.map (lang: {
+      name = "typst-${lang}";
+      value = { config, lib, pkgs, ... }: {
+        imports = [ ./modules/typst.nix ];
+        typstConfig = {
+          enable = true;
+          language = lang;
+        };
+      };
+    }) languages);
+    devShells.${system} = builtins.listToAttrs (pkgs.lib.lists.map (lang: {
+        name = "typst-${lang}";
+        value = pkgs.mkShell {
+          packages = with pkgs; [
+            ltex-ls-plus
+            fd
+            ripgrep
+            (nixvim.legacyPackages.${system}.makeNixvim self.nixosModules."typst-${lang}" ) 
+          ];
+          shellHook = "zsh";
+        };
+     }) languages);
+  };
+}
